f060f87d77aa09997ea8d1ddce08601c3a025aaa
Léo Geison <leogeisom@gmail.com>
2023-03-23 20:34:36 -0300

    Modularizing codes

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,18 +1,4 @@
-export class GithubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`
-
-    return fetch(endpoint)
-      .then(data => data.json())
-      .then(({ login, name, public_repos, followers }) => ({
-        login,
-        name,
-        public_repos,
-        followers
-      }))
-  }
-}
-
+import { GithubUser } from './GithubUser.js'
 // Classe que vai conter a lógica dos dados
 export class Favorites {
   constructor(root) {
